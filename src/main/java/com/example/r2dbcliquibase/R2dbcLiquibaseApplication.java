7343d2c9e0f51f3fc3f7fbedd11c9c434535de15
Viktar Patotski (aka xp-vit) <xp.vit.blr@gmail.com>
2020-03-28 23:24:25 +0100

    initial commit

---
+++ src/main/java/com/example/r2dbcliquibase/R2dbcLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.r2dbcliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/r2dbcliquibase/R2dbcLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r2dbcliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
